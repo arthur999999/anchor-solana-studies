bd2f757436d9dcbb62077564dea67b761cc658b3
arthur999999 <arthurhono.contato@gmail.com>
2024-01-03 17:18:12 -0300

    finish mint token

--- mint-nft-v2/programs/mint-nft-v2/src/lib.rs
+++ mint-nft-v2/programs/mint-nft-v2/src/lib.rs
@@ -21,6 +21,71 @@ declare_id!("GwuQbUd8WeBG92PC5akefN7664eYYhGExaYyjQ25XMow");
 pub mod mint_nft_v2 {
     use super::*;
 
+    pub fn mint_nft(ctx: Context<MintNft>, nft_name: String, nft_symbol: String, nft_uri: String) -> Result<()> {
+
+            msg!("Iniciando o mint");
+            mint_to(
+                CpiContext::new(
+                    ctx.accounts.token_program.to_account_info(),
+                    MintTo { 
+                        mint: ctx.accounts.mint_account.to_account_info(),
+                        to: ctx.accounts.associated_token_account.to_account_info(),
+                        authority: ctx.accounts.payer.to_account_info() }),
+                1
+            )?;
+            msg!("Mint finalizado");
+            msg!("Criando a metadata");
+            create_metadata_accounts_v3(
+                CpiContext::new(
+                    ctx.accounts.,
+                    accounts),
+                data,
+                is_mutable,
+                update_authority_is_signer,
+                details
+            )?;
+        
+        Ok(())
+    }
    
+}
+
+#[derive(Accounts)]
+pub struct MintNft<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    ///CHECK: 
+    #[account(
+        mut,
+        address=find_metadata_account(&mint_account.key()).0
+    )]
+    pub metadata_account: UncheckedAccount<'info>,
+    //CHECK:
+    #[account(
+        mut,
+        address=find_master_edition_account(&mint_account.key()).0
+    )]
+    pub edition_account: UncheckedAccount<'info>,
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 0,
+        mint::authority = payer.key(),
+        mint::freeze_authority = payer.key()
+    )]
+    pub mint_account: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint_account,
+        associated_token::authority = payer
+    )]
+    pub associated_token_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Metadata>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>
 }
 
